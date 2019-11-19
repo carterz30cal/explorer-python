b2b1a62f9c784cd851143da4e5985dbe7f3507a2
carterz30cal <carter.calloway@outlook.com>
2019-11-19 16:53:22 +0000

    Finished Main Menu V1
    added main menu - also added defs for other game functions

--- game.py
+++ game.py
@@ -3,8 +3,31 @@ sys.path.insert(0, os.getcwd() + "/Libs")
 # custom libraries
 import ui
 version = "v0001"
+player_body = []
 
+
+def Tavern():
+    raise Exception("TBA")
+def Settings():
+    raise Exception("TBA")
+def Game():
+    pass
 def MainMenu():
     ui.Menu(["Dive In","Tavern","Settings"],"Ye Olde Menu",True)
-
+    MainMenu_Select()
+def MainMenu_Select():
+    try:
+        selection_int = int(input())
+        if selection_int == 1:
+            Game()
+        elif selection_int == 2:
+            Tavern()
+        elif selection_int == 3:
+            Settings()
+        else:
+            MainMenu_Select()
+    except ValueError:
+        MainMenu_Select()
 MainMenu()
+
+
